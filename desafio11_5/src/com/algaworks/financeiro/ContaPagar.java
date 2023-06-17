bfffe43e3324c8e9644ff20f525a5604f95f9a1e
Rafael Pedroso Meireles <40171101+rafaelmeireles-ti@users.noreply.github.com>
2023-06-17 10:17:41 -0300

    Add files via upload

---
+++ desafio11_5/src/com/algaworks/financeiro/ContaPagar.java
@@ -0,0 +1,73 @@
+package com.algaworks.financeiro;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.io.Serializable;
+
+public class ContaPagar implements Serializable {
+
+    private String descricao;
+    private double valor;
+    private String dataVencimento;
+    private boolean pago;
+    private Fornecedor fornecedor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+
+    public void pagar(){
+        if (isPago()){
+            throw new RuntimeException("Conta ja foi paga");
+        }else {
+            setPago(true);
+        }
+
+    }
+
+    public void cancelarPagamento(){
+        if (isPago()) {
+            setPago(false);
+        } else {
+            throw new RuntimeException("Conta já está pendente de pagamento");
+        }
+
+
+    }
+}
---
+++ desafio11_5/src/com/algaworks/financeiro/Fornecedor.java
@@ -0,0 +1,16 @@
+package com.algaworks.financeiro;
+
+public class Fornecedor {
+    private String nome;
+
+    public Fornecedor (String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
